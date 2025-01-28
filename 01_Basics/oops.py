9d37666e2d854b67c1472fcb0ca9d2ee90484cb3
btwiamsujal <sujalfaganiya2005@gmail.com>
2025-01-28 08:25:37 +0530

    Modify oops.py

--- 01_Basics/oops.py
+++ 01_Basics/oops.py
@@ -13,7 +13,8 @@ class Atm:
       2. press 2 to change pin
       3. press 3 to check balance
       4. press 4 to withdraw
-      5. Anything else to exit
+      5. press 5 to deposit
+      6. Anything else to exit
     """)
     
     if user_input == '1':
@@ -71,6 +72,14 @@ class Atm:
       print("Sale chor")
     self.menu()
     
-  
+  def deposit(self):
+    user_pin = input("Enter your pin")
+    if user_pin == self.pin:
+      amount = int(input("Enter the amount"))
+      self.balance += amount
+      print("Deposit successful. Your balance is:", self.balance)
+    else:
+      print("Pin galat hai")
+    self.menu()
     
 a = Atm()
